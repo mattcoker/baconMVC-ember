4a3756deadf7c2a161dd0bbe155f6eb24d0bc5d1
Matt Coker <matt.james.coker@gmail.com>
2014-11-21 07:52:40 -0600

    Functional listing of 'Posts' on home page

---
+++ app/models/post.js
@@ -0,0 +1,40 @@
+import DS from 'ember-data';
+
+var Post = DS.Model.extend({
+  title: DS.attr('string'),
+  url: DS.attr('string'),
+  timestamp: DS.attr('date'),
+  upvotes: DS.attr('number'),
+  downvotes: DS.attr('number'),
+});
+
+Post.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      title: 'Poetic Systems',
+      url: 'http://poeticsystems.com',
+      timestamp: '1416574195',
+      upvotes: 14,
+      downvotes: 4,
+    },
+    {
+      id: 2,
+      title: 'Transcare',
+      url: 'http://poeticsystems.com',
+      timestamp: '1395574195',
+      upvotes: 12,
+      downvotes: 8,
+    },
+    {
+      id: 3,
+      title: 'HealthQuest EMS',
+      url: 'http://poeticsystems.com',
+      timestamp: '1376574195',
+      upvotes: 5,
+      downvotes: 9,
+    },
+  ]
+});
+
+export default Post;
--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('posts', {path: '/'}, function() {
+    this.resource('post', {path: '/:post_id'});
+  });
+  this.route('add');
 });
 
 export default Router;
