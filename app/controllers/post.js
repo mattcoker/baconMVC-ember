0f6a5cbfa7ba550f37c0649f450b081b4666ac53
Matt Coker <matt.james.coker@gmail.com>
2014-11-27 22:09:58 -0600

    Functional voting for posts

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,9 +1,36 @@
 import Ember from 'ember';
 
 var PostController = Ember.ObjectController.extend({
+  addedVote: 0,
+
+  upVoted: function() {
+    return (this.get('addedVote') === 1);
+  }.property('addedVote'),
+
+  downVoted: function() {
+    return (this.get('addedVote') === -1);
+  }.property('addedVote'),
+
   netVotes: function() {
-    return this.get('model.upvotes') - this.get('model.downvotes');
-  }.property()
+    return this.get('model.upvotes') - this.get('model.downvotes') + this.get('addedVote');
+  }.property('upvotes', 'downvotes', 'addedVote'),
+
+  actions: {
+    addUpVote: function() {
+      if(this.get('addedVote') !== 1) {
+        this.set('addedVote', 1);
+      } else {
+        this.set('addedVote', 0);
+      }
+    },
+    addDownVote: function() {
+      if(this.get('addedVote') !== -1) {
+        this.set('addedVote', -1);
+      } else {
+        this.set('addedVote', 0);
+      }
+    },
+  }
 });
 
 export default PostController;
